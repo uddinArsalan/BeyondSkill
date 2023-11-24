ef0b9b9e12798d3bd14b5fb1149a71377f2405c2
Arsalan <uddinarsalan91@gmail.com>
2023-11-24 15:08:33 +0530

    Deployed the strpe server

--- vite-project/frontend/src/components/Checkout.jsx
+++ vite-project/frontend/src/components/Checkout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Checkout = () => {
   return (
-    <form action="http://localhost:4242/create-checkout-session" method="POST">
+    <form action="https://beyondskill-stripe.onrender.com/create-checkout-session" method="POST">
       <button type="submit" className="text-white bg-green-500 text-xs p-1">
         Enroll now
       </button>
